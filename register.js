8e23be985c913fdce1cc0033b80725972cd66bd1
khadar072 <khadarmoha53@gmial.com>
2024-06-05 08:45:23 +0300

    initial this project

---
+++ register.js
@@ -0,0 +1,11 @@
+
+    function showSignUp() {
+      document.getElementById("signupForm").style.display = "block";
+      document.getElementById("signInForm").style.display = "none";
+    }
+
+    function showSignIn() {
+      document.getElementById("signupForm").style.display = "none";
+      document.getElementById("signInForm").style.display = "block";
+    }
+  
